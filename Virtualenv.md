73fc230f1e379d220e1275608db2229225bccadf
Franky1 <arvid.teichtmann@gmx.de>
2022-07-09 01:33:32 +0200

    some quick refactoring

---
+++ Virtualenv.md
@@ -0,0 +1,17 @@
+# Python **virtualenv** Setup
+
+```shell
+pip install --upgrade virtualenv
+python -m venv venv
+venv\Scripts\activate.bat
+python -m pip install --upgrade pip
+pip install --upgrade wheel setuptools poetry autopep8
+pip install --upgrade jupyter jupyterlab
+pip install --upgrade -r requirements.txt
+# ......
+deactivate.bat
+```
+
+---
+
+> Last updated: 09.07.2022
--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-from datetime import datetime
+import configparser
 import json
+import sys
+from datetime import datetime
+
+import paho.mqtt.client as mqtt
 from lemon import api
 from lemon.market_data.model.quote import Quote
-import paho.mqtt.client as mqtt
-import sys
 
-## Configuration
 
-# Your market data API key: https://docs.lemon.markets/authentication
-api_key = "YOUR-API-KEY"
-
-instruments = [
-    "US0378331005", # Apple
-    "US88160R1014", # Tesla
-]
+# Configuration
+config = configparser.ConfigParser(allow_no_value=True, inline_comment_prefixes=('#',))
+config.optionxform = str  # keep the case of the keys, don't convert to lowercase
+config.read('config.ini')  # read the config file
+api_key = config.get('API', 'key', fallback=None)  # get the api key
+isins = config.items('ISINS')  # get all items from section 'ISINS'
+instruments = [isin[0] for isin in isins]  # get only the isins keys
 
 ## Quotes
-
 quotes = {}
 updates = 0
 spinner = "-\\|/"
+
+
+def get_credentials(api_key : str):
+    print("Fetching credentials for live streaming...")
+    # Lemon Markets API Client
+    lemon_markets_client = api.create(market_data_api_token=api_key, trading_api_token="trading-api-is-not-used", env='paper')
+    try:
+        response = lemon_markets_client.market_data.post("https://realtime.lemon.markets/v1/auth", json={})
+        response = response.json()
+        user_id = response['user_id']
+        token = response['token']
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+    return lemon_markets_client, user_id, token
+
 
 def print_quotes():
     print("\r", end="")
@@ -31,28 +47,26 @@ def print_quotes():
     print(spinner[updates % len(spinner)], end="")
     sys.stdout.flush()
 
-## Lemon Markets API Client
 
-lm_client = api.create(market_data_api_token=api_key, trading_api_token="trading-api-is-not-used")
-
-## Live Streaming Callbacks
-
+# Live Streaming Callback Functions
 def on_connect(mqtt_client, userdata, flags, rc):
-    print(f"Connected.   Subscribing to {user_id}…")
+    print(f"Connected.   Subscribing to {user_id}")
     mqtt_client.subscribe(user_id)
-    
+
+
 def on_subscribe(mqtt_client, userdata, level, buff):
-    print(f"Subscribed.  Publishing requested instruments to {user_id}.subscriptions…")
+    print(f"Subscribed.  Publishing requested instruments to {user_id}.subscriptions...")
     mqtt_client.publish(f"{user_id}.subscriptions", ",".join(instruments))
     # This is a great place to fetch `/v1/quotes/latest` on time via REST, so
     # you have _all_ the latest quotes and you can update them via incoming
     # live stream messages.
-    print("Published.   Fetching latest quotes for initialization…")
-    latest = lm_client.market_data.quotes.get_latest(isin=instruments, epoch=True, decimals=False)
+    print("Published.   Fetching latest quotes for initialization...")
+    latest = lemon_markets_client.market_data.quotes.get_latest(isin=instruments, epoch=True, decimals=False)
     for quote in latest.results:
         quotes[quote.isin] = quote
-    print("Initialized. Waiting for live stream messages…")
+    print("Initialized. Waiting for live stream messages...")
     print_quotes()
+
 
 def on_message(client, userdata, msg):
     global updates
@@ -62,24 +76,20 @@ def on_message(client, userdata, msg):
     updates += 1
     print_quotes()
 
-## Request Live Streaming Credentials
 
-print("Fetching credentials for live streaming…")
+if __name__ == "__main__":
+    # Request Live Streaming Credentials
+    lemon_markets_client, user_id, token = get_credentials(api_key)
 
-response = lm_client.market_data.post("https://realtime.lemon.markets/v1/auth", json={}).json()
-user_id = response['user_id']
-token = response['token']
+    # Prepare Live Streaming Connection
+    mqtt_client = mqtt.Client("Ably_Client")
+    mqtt_client.username_pw_set(username=token)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.on_subscribe = on_subscribe
 
-## Prepare Live Streaming Connection
-
-mqtt_client = mqtt.Client("Ably_Client")
-mqtt_client.username_pw_set(username=token)
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
-mqtt_client.on_subscribe = on_subscribe
-
-## Connect
-
-print("Fetched.     Connecting MQTT client…")
-mqtt_client.connect("mqtt.ably.io")
-mqtt_client.loop_forever()
+    ##Connect and receive data
+    print("Fetched.     Connecting MQTT client...")
+    mqtt_client.connect("mqtt.ably.io")
+    mqtt_client.loop_forever()
+    print("Disconnected.")
