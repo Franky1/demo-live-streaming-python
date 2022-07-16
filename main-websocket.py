8a0202d588458c7370e14e65c3ac365d1f20008d
Franky1 <arvid.teichtmann@gmx.de>
2022-07-16 19:36:03 +0200

    added logging

--- main-websocket.py
+++ main-websocket.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+from cmath import log
 
 import configparser
 import json
+import logging
 import os
 import sys
 import time
@@ -37,6 +39,11 @@ class WsActions(Enum):
     AUTH = 17
 
 
+# config logging
+logfmt = '%(asctime)s : %(levelname)7s : %(message)s'
+logging.basicConfig(level=logging.INFO, format=logfmt)
+
+
 # Global variable with latest Quotes
 quotes: dict = {}
 instruments: list = []
@@ -52,7 +59,8 @@ def load_config() -> tuple[str, list]:
     config = configparser.ConfigParser(
         # Allow keys without values for the isins, allow inline comments
         allow_no_value=True, inline_comment_prefixes=('#', ';'))
-    config.optionxform = str  # keep the case of the keys, don't convert to lowercase
+    # keep the case of the keys, don't convert to lowercase:
+    config.optionxform = str  # type: ignore
     config.read('config.ini')  # read the config file
     if not api_key:  # if no API key is provided in env, read it from the config file
         api_key = config.get('API', 'LEMON_API_KEY', fallback=None)  # get the api key
@@ -60,15 +68,18 @@ def load_config() -> tuple[str, list]:
     instruments = [isin[0] for isin in isins]  # get only the isins keys
 
     if not api_key:
-        print("No API key found. Please set LEMON_API_KEY in env variable or .env or config.ini")
+        logging.error("No API key found. Please set LEMON_API_KEY in env variable or .env or config.ini")
         sys.exit(1)
 
     return api_key, instruments
 
 
 def get_credentials(api_key: str)-> tuple[api.Api, str, str, datetime]:
-    '''Get the token for the MQTT broker connection from the Lemons REST API auth endpoint.'''
-    print("Fetching.    Credentials for live streaming...")
+    '''Get the token for the MQTT broker connection from the Lemons REST API auth endpoint.
+    Use `expires_at` to reconnect, because this connection will stop.
+    receiving data: https://docs.lemon.markets/live-streaming/overview#stream-authorization
+    '''
+    logging.info("Fetching credentials for live streaming...")
     # Lemon Markets API Client
     lemon_markets_client = api.create(
         market_data_api_token=api_key, trading_api_token="trading-api-is-not-used", env='paper')
@@ -76,13 +87,11 @@ def get_credentials(api_key: str)-> tuple[api.Api, str, str, datetime]:
         response = lemon_markets_client.market_data.post(
             "https://realtime.lemon.markets/v1/auth", json={})
         response = response.json()
-        # **NOTE:** Use `expires_at` to reconnect, because this connection will stop
-        # receiving data: https://docs.lemon.markets/live-streaming/overview#stream-authorization
         expires_at = datetime.fromtimestamp(response['expires_at'] / 1000)
         user_id = response['user_id']
         token = response['token']
     except Exception as e:
-        print(f"Error: {e}")
+        logging.error(f"Error: {e}")
         sys.exit(1)
     return lemon_markets_client, user_id, token, expires_at
 
@@ -91,7 +100,7 @@ def check_token(api_key: str, expires_at: datetime):
     '''Check if the token is still valid, one hour before expiration.'''
     now = datetime.now() + timedelta(hours=1)
     if now > expires_at:
-        print("Token expired. Fetching new token...")
+        logging.warn("Token expired. Fetching new token...")
         lemon_markets_client, user_id, token, expires_at = get_credentials(api_key)
     return lemon_markets_client, user_id, token, expires_at
 
@@ -101,12 +110,12 @@ def print_quote(quote) -> None:
     ask = format(quote.a / 10000, ".4f")
     bid = format(quote.b / 10000, ".4f")
     date = datetime.fromtimestamp(quote.t / 1000.0).isoformat(timespec='milliseconds')
-    print(f"{quote.isin} (exc={quote.mic}, ask={ask}, bid={bid}, date={date})")
+    logging.info(f"{quote.isin} (exc={quote.mic}, ask={ask}, bid={bid}, date={date})")
 
 
 def attach_channel(ws: websocket.WebSocketApp, user_id: str) -> None:
     '''Attaching to my channel.'''
-    print("Attaching to channel...")
+    logging.info("Attaching to channel...")
     ws.send(json.dumps({
         'action': WsActions.ATTACH.value,
         'channel': user_id,
@@ -115,7 +124,7 @@ def attach_channel(ws: websocket.WebSocketApp, user_id: str) -> None:
 
 def detach_channel(ws: websocket.WebSocketApp, user_id: str) -> None:
     '''Detaching from my channel.'''
-    print("Detaching from channel...")
+    logging.info("Detaching from channel...")
     ws.send(json.dumps({
         'action': WsActions.DETACH.value,
         'channel': user_id,
@@ -124,7 +133,7 @@ def detach_channel(ws: websocket.WebSocketApp, user_id: str) -> None:
 
 def close(ws: websocket.WebSocketApp, user_id: str) -> None:
     '''Close the websocket connection gracefully.'''
-    print("Closing connection...")
+    logging.info("Closing connection...")
     ws.send(json.dumps({
         'action': WsActions.CLOSE.value,
         'channel': user_id,
@@ -133,7 +142,7 @@ def close(ws: websocket.WebSocketApp, user_id: str) -> None:
 
 def publish_message(ws: websocket.WebSocketApp, user_id: str, instruments: list) -> None:
     '''Publish message. Subscribe to all instruments.'''
-    print("Publish message...")
+    logging.info("Publish message...")
     ws.send(json.dumps({
         'action': WsActions.MESSAGE.value,
         'channel': f'{user_id}.subscriptions',
@@ -153,45 +162,36 @@ def on_message(ws, message):
     # decoded_data = msgpack.loads(message)
     decoded_data = json.loads(message)
     action = decoded_data.get('action', None)
-    print(f"{WsActions(action).name} received")
-    print(f'{WsActions(action).name} : {decoded_data}')
-    if action == WsActions.HEARTBEAT.value:
-        pass
-    elif action == WsActions.NACK.value:
-        pass
-    elif action == WsActions.CONNECTED.value:
+    logging.debug(f"{WsActions(action).name} received")
+    logging.debug(f'{WsActions(action).name} : {decoded_data}')
+
+    if action == WsActions.CONNECTED.value:
         attach_channel(ws, user_id)
     elif action == WsActions.ATTACHED.value:
         publish_message(ws, user_id, instruments)
-    elif action == WsActions.DETACHED.value:
-        pass
-    elif action == WsActions.SYNC.value:
-        pass
-    else:
-        pass
 
 
 def on_error(ws, error):
-    print(f'Error: {error}')
+    logging.error(f'Error: {error}')
 
 
 def on_close(ws, close_status_code, close_msg):
-    print("Closed connection")
+    logging.info("Closed connection")
 
 
 def on_open(ws):
-    print("Opened connection")
+    logging.info("Opened connection")
 
 
 def on_pong(ws, pong_data):
-    print(f'Pong: {pong_data}')
+    logging.debug(f'Pong: {pong_data}')
 
 
 def on_data(ws, data, datatype, flag):
     # decoded_data = msgpack.loads(data)
     decoded_data = json.loads(data)
-    print(f'Datatype: {datatype}')
-    print(f'Data: {decoded_data}')
+    logging.debug(f'Datatype: {datatype}')
+    logging.debug(f'Data: {decoded_data}')
 
 
 # so far the connection with websockets is working, but the subscription is not working
@@ -201,7 +201,7 @@ if __name__ == "__main__":
 
     # Request Live Streaming Credentials
     lemon_markets_client, user_id, token, expires_at = get_credentials(api_key)
-    print(f"Fetched.     Token expires at {expires_at.isoformat()}")
+    logging.info(f"Fetched Token. Token expires at {expires_at.isoformat()}")
 
     # Prepare Live Streaming Connection
     # websocket.enableTrace(True)  # enable for debugging
@@ -209,9 +209,9 @@ if __name__ == "__main__":
     ws = websocket.WebSocketApp(url=f'wss://realtime.ably.io?client_id={user_id}&access_token={token}&format=json&heartbeats=true',
                                 on_open=on_open,
                                 on_message=on_message,
-                                # on_data=on_data,
                                 on_error=on_error,
                                 on_close=on_close,
+                                # on_data=on_data,
                                 # on_pong=on_pong,
                                 keep_running=False)
 
@@ -222,12 +222,12 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         # Close the websocket connection gracefully
         # However this does not work...
-        print("KeyboardInterrupt...")
+        logging.warn("KeyboardInterrupt...")
         detach_channel(ws, user_id)
         time.sleep(1)
         close(ws, user_id)
         time.sleep(1)
     finally:
         ws.close()
-        print("Disconnected. Exiting...")
+        logging.info("Disconnected. Exiting...")
         sys.exit(0)
